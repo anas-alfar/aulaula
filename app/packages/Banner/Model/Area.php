16f2e8db773427f964d2aa881dcc57b2be766cda
Mohammad Mousa <mohammad@mohammad.(none)>
2012-06-08 02:03:29 +0300

    add doc block

--- app/packages/Banner/Model/Area.php
+++ app/packages/Banner/Model/Area.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+*
+* Aulaula
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0) that is
+* bundled with this package in the file LICENSE.txt. It is also available through
+* the world-wide-web at this URL: http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to obtain it
+* through the world-wide-web, please send an email to license@aulaula.com
+* so we can send you a copy immediately.
+*
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade Aulaula to newer versions
+* in the future. If you wish to customize Magento for your needs please refer to
+* http://www.aulaula.com for more information.
+*
+* @category Aula
+* @package Aula_Banner_Model_Area
+* @copyright Copyright (c) 2012 Aulaula (http://www.aulaula.com/)
+* @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+* @author Anas K. Al-Far <anas@al-far.com>
+*
+*/
+
 class Banner_Model_Area extends Aula_Model_DbTable {
 	
 	protected $_name = 'banner_area';
--- app/packages/Landlots/Controller/AncillaryBuildingsAdmin.php
+++ app/packages/Landlots/Controller/AncillaryBuildingsAdmin.php
@@ -1,5 +1,35 @@
 <?php
 
+/**
+ *
+ * Aulaula
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the file LICENSE.txt. It is also available through
+ * the world-wide-web at this URL: http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email to license@aulaula.com
+ * so we can send you a copy immediately.
+ *
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Aulaula to newer versions
+ * in the future. If you wish to customize Magento for your needs please refer to
+ * http://www.aulaula.com for more information.
+ *
+ * @category Aula
+ * @package Aula_Landlots
+ * @subpackage Controller
+ * @name Landlots_Controller_AncillaryBuildingsAdmin
+ * @copyright Copyright (c) 2012 Aulaula (http://www.aulaula.com/)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @author Anas K. Al-Far <anas@al-far.com>
+ *
+ */
+
 class Landlots_Controller_AncillaryBuildingsAdmin extends Aula_Controller_Action {
 
 	private $ancillaryBuildingsObj = NULL;
@@ -28,13 +58,13 @@ class Landlots_Controller_AncillaryBuildingsAdmin extends Aula_Controller_Action
 		}
 		$this -> view -> sanitized['token']['value'] = md5(time() . 'qwiedkhjsafg');
 		$this -> view -> sanitized['locale']['value'] = 1;
-		
+
 		$this -> view -> importExcelLink = '/admin/handle/pkg/landlots-ancillary-buildings/action/importcsv/';
 		$this -> view -> exportExcelLink = '/admin/handle/pkg/landlots-ancillary-buildings/action/exportcsv/';
 	}
 
 	public function viewAction() {
-		if ( isset($_GET['id']) and is_numeric($_GET['id']) ) {
+		if (isset($_GET['id']) and is_numeric($_GET['id'])) {
 			$result = $this -> ancillaryBuildingsObj -> getAncillaryBuildingsById($_GET['id']);
 			$this -> view -> result = $result;
 			$this -> view -> render('landlots/viewAncillaryBuildings.phtml');
